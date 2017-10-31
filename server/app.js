83f1759dd1e3db2cfef866b9a48939059c4a06f1
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-31 13:49:33 -0700

    Create and link index.html and styles.css files inside of public directory. Serve public/dist files with express. #1

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
+app.use(express.static('./server/public/dist/'))
 app.use(bodyParser.json())
 
 app.listen(3043, () => {
