48e20fce61fc98be265c8d4ab0ef57e15393b6b9
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-10-31 15:41:26 -0700

    Make get and use request to get /images, /description, and /names. #1

--- server/app.js
+++ server/app.js
@@ -6,6 +6,30 @@ const app = express()
 app.use(express.static('./server/public/dist/'))
 app.use(bodyParser.json())
 
+app.get('/images', (req, res) => {
+  request('https://techi.envivent.com/images.json', (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    res.send((JSON.parse(body)))
+  })
+})
+
+app.get('/descriptions', (req, res) => {
+  request('https://techi.envivent.com/description.json', (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    res.send((JSON.parse(body)))
+  })
+})
+
+app.get('/names', (req, res) => {
+  request('https://techi.envivent.com/names.json', (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    res.send((JSON.parse(body)))
+  })
+})
+
 app.listen(3043, () => {
   console.log('Listening on port 3043')
 })
